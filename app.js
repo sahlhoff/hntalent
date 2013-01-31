73335faefc2ec361a4f17d4c2da049cd58efe47c
Chad Sahlhoff <csahlhoff@gmail.com>
2013-01-31 15:58:53 -0800

    initial commit

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ var express = require('express')
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || 5000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
+  app.use(express.cookieParser('iloveturtles'));
   app.use(express.session());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
